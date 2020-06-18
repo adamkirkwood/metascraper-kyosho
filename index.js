8c335133d451fa42daf4cdf04a38d44516710f54
adamkirkwood <hello@adamkirkwood.com>
2020-06-17 22:28:15 -0700

    test URLs

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const regex = /In Stock/g;
 
+const REGEX_KYOSHOAMERICA_URL = /(https?:\/\/(.+?\.)?kyoshoamerica\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/
+
+const isValidUrl = memoizeOne(({ url }) => REGEX_KYOSHOAMERICA_URL.test(url))
+
 /**
  * A set of rules we want to declare under `metascraper-kyosho` namespace.
  *
@@ -11,5 +15,8 @@ module.exports = () => {
     availability: ({ htmlDom: $, url }) => (($('div#availability').text().matchAll(regex) === null) || false),
     quantity: ({ htmlDom: $, url }) => 0
   }
+
+  rules.test = isValidUrl
+
   return rules
 }
