4a5cc4ac340f8a3f8f97860cf681302baa0ac024
adamkirkwood <hello@adamkirkwood.com>
2020-06-17 20:14:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const regex = /In Stock/g;
+
+/**
+ * A set of rules we want to declare under `metascraper-kyosho` namespace.
+ *
+**/
+module.exports = () => {
+  const rules = {
+    availability: ({ htmlDom: $, url }) => ($('div#availability').text().match(regex) === null)
+  }
+  return rules
+}
